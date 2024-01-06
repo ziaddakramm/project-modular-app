d9c288ac8c0154a9ec66df933b168809f06e8348
ziaddakramm <106281067+ziaddakramm@users.noreply.github.com>
2024-01-06 21:06:23 +0200

    Header out of core module

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { AuthService } from '../../auth/auth-service';
+import { AuthService } from '../auth/auth-service';
 
 
 @Component({
